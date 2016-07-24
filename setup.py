7fb9753d7c2073883ca4375d18bff4be7ce0a98c
3DLIRIOUS <3dlirious@gmail.com>
2016-07-24 13:44:03 -0500

    Updated version to 134.20160724

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='MeshLabXML',
-      version='134.20160616',
+      version='134.20160724',
       description='Create and run MeshLab XML scripts',
       url='https://github.com/3DLIRIOUS/MeshLabXML',
       author='3DLirious',
       author_email='3DLirious@gmail.com',
       license='LGPL-2.1',
-      packages=['meshlabxml'])
+      packages=['meshlabxml'],
+      include_package_data=True)
